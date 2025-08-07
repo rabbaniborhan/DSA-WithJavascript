4c7b0d2d5e3d8a7bf238df6185e91f00a06a23c6
Borhan <brborhan70@gmail.com>
2025-08-07 22:00:52 +0600

    the algorithms is recursive fibonacci

--- Algorithms/Recursive-Fibonacci.js
+++ Algorithms/Recursive-Fibonacci.js
@@ -3,4 +3,10 @@ function fibonacciRecursive(n) {
   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-console.log(fibonacciRecursive(6));
+console.log(fibonacciRecursive(10));
+
+// 🧠 Big O Complexity
+// Time Complexity: O(2^n) → exponential
+// Because it splits into 2 recursive calls at each level.
+
+// Space Complexity: O(n) → due to the recursive call stack.
