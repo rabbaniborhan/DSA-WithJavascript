c0e09218b5ea360a8081a9002a3527824e5b29cc
Borhan <brborhan70@gmail.com>
2025-08-08 22:58:50 +0600

    The algorithms is Bubble Sort

---
+++ Algorithms/BubbleSort.js
@@ -0,0 +1,18 @@
+const bubbleSort = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+
+// Example
+console.log(bubbleSort([5, 3, 8, 4, 2]));
