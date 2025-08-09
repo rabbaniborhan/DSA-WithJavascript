53fee38dd647f40cbe7bd65ab8f4562d3967a109
Borhan <brborhan70@gmail.com>
2025-08-09 06:20:20 +0600

    The algorithms is Insertion sort

--- Algorithms/InsertionSort.js
+++ Algorithms/InsertionSort.js
@@ -1,21 +1,17 @@
-const bubbleSort = (arr) => {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
+const InsertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
-  } while (swapped);
+    arr[j + 1] = numberToInsert;
+  }
   return arr;
 };
 
-// Example
-console.log(bubbleSort([5, 3, 8, 4, 2]));
+console.log(InsertionSort([5, 3, 8, 4, 2]));
 
 // 📊 Big O Complexity
 
